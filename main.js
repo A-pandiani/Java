1721ac73a0bc25adc2508957f8e611ff19dbdeb6
Antonio Pandiani <a_pandiani@hotmail.com>
2021-11-08 12:16:14 -0300

    desafio clase 3 Antonio pandiani

---
+++ main.js
@@ -0,0 +1,50 @@
+//let coder = 0;
+//while (coder < 10){
+//   console.log("Hola coder") 
+//  coder ++
+//}
+//for(let i = 0; i <= 9; i++){
+//  if(i==5){
+//    alert( "voy por la mitad")
+//}
+//console.log("Hola Coder")
+//}
+
+//let nombre= prompt("Ingresa tu nombre");
+//while(nombre ==""){
+// nombre = prompt("Ingresa tu nombre");
+//}
+//alert("Gracias " + nombre);
+let numero = prompt("Ingresa el numero ganador!");
+switch (numero) {
+    case "1":
+        alert( numero +" es incorrecto");
+        break;
+    case "2":
+        alert( numero +" es incorrecto");
+        break;
+    case "3":
+        alert( numero +" es incorrecto");
+        break;
+    case "4":
+        alert( numero +" es incorrecto");
+        break;
+    case "5":
+        alert( numero +" es incorrecto");
+        break;
+    case "6":
+        alert( numero +" es incorrecto");
+        break;
+    case "7":
+        alert("Correcto, el numero ganador es " + numero);
+        break;
+    case "8":
+        alert( numero +" es incorrecto");
+        break;
+    case "9":
+        alert( numero +" es incorrecto");
+        break;
+    case "10":
+        alert( numero +" es incorrecto");
+        break;
+}
