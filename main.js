28d0b8f782b7b808169bf41b2e73b65bce1a1038
Antonio Pandiani <a_pandiani@hotmail.com>
2021-11-22 13:48:36 -0300

    Desafio Arrays.

--- main.js
+++ main.js
@@ -1,71 +1,13 @@
-const fernet = 800;
-const coca = 200;
-const hielo = 200;
-const vodka = 700;
-const speed = 300;
-const gin = 1500;
-const sprite = 200;
-const envio = 80;
+//ARRAYS DE BEBIDAS//
 
 
-let respuestaClave = prompt("Quiere fernet , vodka o gin?") 
+const bebidas =[
+    {id: "fernet", value:1400, ccl:1000},{id: "vodka", value:1300, ccl:950}, {id:"gin", value: 1500, ccl:900}
+];
+console.log(bebidas);
 
-function sumarFernet(fernet,coca,hielo) {
-    const resultado = (fernet + coca + hielo)
-    
-    return resultado
-   
-}
+//ARRAY DE TABACOS
 
-
-function sumarVodkas(vodka,speed,hielo){
- const resultadoUno = (vodka + speed + hielo)
-  return resultadoUno
-   
-}
-
-function sumarGin(gin,sprite,hielo){
-    const resultadoGin = (gin + sprite + hielo)
-    return resultadoGin
-}
-
-function sumarEnvio(envio){
-    const sumaEnvio =(envio)
-    return sumaEnvio
-}
-
-
-
-if(respuestaClave == "fernet" ){
-    alert("El total es $"+sumarFernet(fernet,coca,hielo));
-    let respuestaEnvio =prompt("¿Le gustaria añadir envio?")
-    if(respuestaEnvio == "si"){
-        alert("Le quedaria $"+ parseInt(sumarFernet(fernet,coca,hielo)+ sumarEnvio(envio)));
-    }else{alert("Bueno, entonces serian: $" + sumarFernet(fernet,coca,hielo));}
-
-}
-
-
-   
-
-if(respuestaClave == "vodka"){
-    alert("El total es $"+sumarVodkas(vodka,speed,hielo));
-    let respuestaEnvio =prompt("¿Le gustaria añadir envio?")
-    if(respuestaEnvio == "si"){
-        alert("Le quedaria $"+ parseInt(sumarVodkas(vodka,speed,hielo)+ sumarEnvio(envio)));
-    }else{alert("Bueno, entonces serian: $" + sumarVodkas(vodka,speed,hielo));}
-
-}
-
-    
-
-
-
-if(respuestaClave == "gin"){
-    alert("El total es $"+sumarGin(gin,sprite,hielo));
-    let respuestaEnvio =prompt("¿Le gustaria añadir envio?")
-    if(respuestaEnvio == "si"){
-        alert("Le quedaria $"+ parseInt(sumarGin(gin,sprite,hielo)+ sumarEnvio(envio)));
-    }else{alert("Bueno, entonces serian: $" + sumarGin(gin,sprite,hielo));}
-
-}
+const tabacos =[
+    {name: "marlboro", value:280,size:20},{name:"philips", value:290,size:20},{name:"lucky", value: 170, size:10}
+];
